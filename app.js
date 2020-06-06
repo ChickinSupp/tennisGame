cdef5877d6d77c764631c94945416b683d5c58bb
O Padeiro <daviiddamiianbaker@gmail.com>
2020-06-06 18:05:35 -0400

    adding some content

--- app.js
+++ app.js
@@ -1,5 +1,23 @@
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
+let canvas;
+let ctx;
 
-ctx.fillStyle = 'black';
-ctx.fillRect(0, 0, canvas.width, canvas.height);
+window.onload = function () {
+
+   canvas = document.getElementById('canvas');
+   ctx = canvas.getContext('2d');
+  
+    drawEverything()
+};
+
+function drawEverything() {
+    ctx.fillStyle = 'black';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    ctx.fillStyle = 'white';
+    ctx.fillRect(225,210,200,200)
+
+    ctx.fillStyle = 'red';
+    ctx.fillRect(canvas.width/2,200,50,25)
+};
+
+console.log(canvas);
