6f7a47bb7681ef4e1ed30ac213eff7885a5203a7
O Padeiro <daviiddamiianbaker@gmail.com>
2020-06-07 18:47:54 -0400

    getting ball to bounce back and fourth

--- app.js
+++ app.js
@@ -1,35 +1,36 @@
 let canvas;
 let ctx;
 let ballX = 50;
-
-
-
+let ballSpeedX = 15;
 
 window.onload = function () {
+  canvas = document.getElementById("canvas");
+  ctx = canvas.getContext("2d");
 
-   canvas = document.getElementById('canvas');
-   ctx = canvas.getContext('2d');
-
-   let fps =  30;
-   setInterval( () => {
+  let fps = 30;
+  setInterval(() => {
     moveEverything();
     drawEverything();
-   }, 1000/fps)
-   
+  }, 1000 / fps);
 };
 
 function moveEverything() {
-    ballX = ballX + 5;
+  ballX = ballX + ballSpeedX;
+  if(ballX < 0) {
+    ballSpeedX = -ballSpeedX;
+  }
+  if(ballX > canvas.width) {
+    ballSpeedX = -ballSpeedX
+  }
 }
 
 function drawEverything() {
+  ctx.fillStyle = "black";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-    ctx.fillStyle = 'black';
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = "white";
+  ctx.fillRect(0, 210, 10, 100);
 
-    ctx.fillStyle = 'white';
-    ctx.fillRect(0,210,10,100)
-
-    ctx.fillStyle = 'red';
-    ctx.fillRect(ballX,100,10,10)
-};
+  ctx.fillStyle = "red";
+  ctx.fillRect(ballX, 100, 10, 10);
+}
