084ff66ed8374b633511ab7768d04170319ca094
O Padeiro <daviiddamiianbaker@gmail.com>
2020-06-10 18:40:03 -0400

    ball working

--- app.js
+++ app.js
@@ -1,58 +1,48 @@
 let canvas;
-let ctx;
+let canvasContext;
 let ballX = 50;
 let ballSpeedX = 10;
 
-window.onload = function () {
-  canvas = document.getElementById('canvas');
-  ctx = canvas.getContext('2d');
+window.onload = function() {
+	canvas = document.getElementById('canvas');
+	canvasContext = canvas.getContext('2d');
 
-  let fps = 30;
-  setInterval(() => {
-    moveEverything();
-    drawEverything();
-  }, 1000 / fps);
-};
-
-
-// MOVES BALL BACK AND FOURTH
-function moveEverything() {
-  ballX = ballX + ballSpeedX;
-  if(ballX < 0) {
-    ballSpeedX = -ballSpeedX;
-  }
-  if(ballX > canvas.width) {
-    ballSpeedX = -ballSpeedX
-  }
+	let framesPerSecond = 30;
+	setInterval(function() {
+			moveEverything();
+			drawEverything();	
+		}, 1000/framesPerSecond);
 }
 
-// THIS FUNCTION IS CALLED IN OUR
-// drawEverything() TO DRAW OUR CANVAS AND GAME COMPONENTS
-function colorRect(leftX,topY,width,height,drawColor) {
-    ctx.fillStyle = drawColor;
-    ctx.fillRect(leftX,topY,width,height);
-  
+function moveEverything() {
+	ballX = ballX + ballSpeedX;
+	if(ballX < 0) {
+		ballSpeedX = -ballSpeedX;
+	}
+	if(ballX > canvas.width) {
+		ballSpeedX = -ballSpeedX;
+	}
 }
 
 function drawEverything() {
-    // GAME CANVAS
-    colorRect(0,0,canvas.width, canvas.height,'#000')
+	// next line blanks out the screen with black
+	colorRect(0,0,canvas.width,canvas.height,'#000');
 
-    // LEFT PADDLE
-    colorRect(0, 210, 10, 100,'#fff');
+	// this is left player paddle
+	colorRect(0,210,10,100,'#fff');
 
-    // BALL
-    ctx.fillStyle = '#fff';
-    // ctx.fillRect(ballX,50,10,10)
-    ctx.beginPath();
-    ctx.arc(ballX, 100 ,10 ,0 ,Math.PI*2 ,true);
-    // stroke();
-    colorCircle(ballX,150,10,'white')
+	// next line draws the ball
+	colorCircle(ballX, 150, 10, 'white');
 }
 
-function colorCircle(centerX,centerY,radius,drawColor) {
-    ctx.fillStyle = drawColor;
-    ctx.beginPath();
-    ctx.arc(centerX,centerY,radius,0,Math.PI*2 ,true);
-    // stroke();
+function colorCircle(centerX, centerY, radius, drawColor) {
+	canvasContext.fillStyle = drawColor;
+	canvasContext.beginPath();
+	canvasContext.arc(centerX, centerY, radius, 0,Math.PI*2,true);
+	canvasContext.fill();
+}
+
+function colorRect(leftX,topY, width,height, drawColor) {
+	canvasContext.fillStyle = drawColor;
+	canvasContext.fillRect(leftX,topY, width,height);
 }
