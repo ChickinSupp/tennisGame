7dd3af1b36021876d31795d0f01cdcb9089c869c
O Padeiro <daviiddamiianbaker@gmail.com>
2020-06-10 19:28:33 -0400

    paddle not moving

--- app.js
+++ app.js
@@ -5,7 +5,19 @@ let ballY = 50;
 let ballSpeedX = 10;
 let ballSpeedY = 4;
 
+let paddleOneY = 250;
+const PADDLE_HEIGHT = 100;
 
+function calculateMousePos(evt) {
+  let rect = canvas.getBoundingClientRect();
+	let root = document.documentElement;
+	let mouseX = evt.clientX - rect.left - root.scrollLeft;
+	let mouseY = evt.clientY - rect.top - root.scrollTop;
+	return {
+		x:mouseX,
+		y:mouseY
+	};
+}
 
 window.onload = function() {
 	canvas = document.getElementById('canvas');
@@ -15,14 +27,21 @@ window.onload = function() {
 	setInterval(function() {
 			moveEverything();
 			drawEverything();	
-		}, 1000/framesPerSecond);
+    }, 1000/framesPerSecond);
+    
+    canvas.addEventListener('mousemove',
+		function(evt) {
+			let mousePos = calculateMousePos(evt);
+			paddle1Y = mousePos.y;
+    });
+    console.log(calculateMousePos)
 }
 
 function moveEverything() {
   ballX = ballX + ballSpeedX;
   ballY= ballY + ballSpeedY;
 
-	if(ballX < 0) {
+  if(ballX < 0) {
 		ballSpeedX = -ballSpeedX;
 	}
 	if(ballX > canvas.width) {
@@ -34,14 +53,17 @@ function moveEverything() {
 	if(ballY > canvas.height) {
 		ballSpeedY = -ballSpeedY;
 	}
+
 }
+
+
 
 function drawEverything() {
 	// next line blanks out the screen with black
 	colorRect(0,0,canvas.width,canvas.height,'#000');
 
 	// this is left player paddle
-	colorRect(0,210,10,100,'#fff');
+	colorRect(0,paddleOneY,10,100,'#fff');
 
 	// next line draws the ball
 	colorCircle(ballX, ballY, 10, '#fff');
